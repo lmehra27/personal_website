3e10d088718a9002bcd389cc60efcfe6fb7e4fcf
Lucky Mehra <mehra27lucky@gmail.com>
2025-05-19 12:12:00 -0500

    added site url

--- docs/publishconf.py
+++ docs/publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ""
+SITEURL = "https://lmehra27.github.io/personal_website"
 RELATIVE_URLS = True
 
 # FEED_ALL_ATOM = "feeds/all.atom.xml"
